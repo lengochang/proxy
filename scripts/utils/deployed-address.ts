0a17c299f3d26b6e357de46a67af380c6d7042a3
lengochang <ngochang.hutech@gmail.com>
2024-08-17 17:28:59 +0700

    first commit

---
+++ scripts/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xE3F87f931F09160ba1C2C95f9Cf1ECd75bD4E3c0'
+export const ProxyAdmin = '0x3720da89B64b0aee8440daa120d6064b47364fD1'
+export const SWTRImplementation = '0xd5B4cc966C8e6A1F5E43d45552C296DfDA575981'
+    
